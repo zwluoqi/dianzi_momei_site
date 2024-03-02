e25f782af38f32bef91f4831294937b50f60b0af
xishengbo <xishengbo@baidu.com>
2024-03-02 17:42:56 +0800

    24-03-02 改用ejs输出html-便于携带后端数据

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8800;
 
+// 设置模板引擎
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 登入页面
 app.get('/signin', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public/signin.html'));
+    res.render('signin', {title: 'Hey'});
 });
 // 账户页面
 app.get('/account', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public/account.html'));
+    // 如果没有登入需要redirec到登入页面
+    res.render('account', {title: 'Hey'});
 });
 // 选择进入页面
 app.get('/select', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public/select.html'));
+    // 如果没有登入需要redirec到登入页面
+    res.render('select', {title: 'Hey'});
 });
 
 // 启动服务，监听 3000 端口
