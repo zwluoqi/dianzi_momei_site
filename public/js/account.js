2971bbfd3b5d0db9d05612810c050ccf802ba5b1
xishengbo <xishengbo@baidu.com>
2024-03-03 16:02:46 +0800

    添加username email api 的更新组件和js事件交互

--- public/js/account.js
+++ public/js/account.js
@@ -1,1 +1,18 @@
-console.log('this is login.')
+const wrapper = document.querySelector('.container');
+// 点击事件委托
+wrapper.addEventListener('click', function(e){
+    const target = e.target;
+    const matchNames = ['username', 'email', 'api'];
+    const name = target.name;
+    const idx = name ? matchNames.indexOf(name) : false;
+    console.log(name, 'idx', idx);
+    if (idx > -1) {
+        // update api
+        if (idx === 2) {
+            
+        }
+        else {
+            // update name email
+        }
+    }
+});
