3289a5ae49612213e3b6145a3ad1622fbb4aa9be
xishengbo <xishengbo@baidu.com>
2024-03-03 13:39:27 +0800

    24-03-03 signin支持了登入状态

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // 导入 express 模块
 const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 // 创建 express 应用
 const app = express();
 const port = process.env.PORT || 8800;
+app.use(session({
+    secret: 'XNMSADJK', // 用于签名会话ID的密钥
+    resave: true, // 强制将会话保存到会话存储中，即使在请求期间会话没有被修改
+    saveUninitialized: false, // 强制将未初始化的会话保存到存储中。新的未修改的会话被视为未初始化。
+    cookie: { secure: false } // 如果设置为true，则仅在HTTPS连接上发送cookie
+}));
 
 // 设置模板引擎
 app.set('view engine', 'ejs');
@@ -12,28 +20,67 @@ app.set('views', path.join(__dirname, 'views'));
 
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
+// 解析application/json 类型的请求体
+app.use(bodyParser.json());
 
 // 登入页面
 app.get('/signin', function(req, res) {
-    res.render('signin', {title: '登录'});
+    if (!req.session.user) {
+        res.render('signin', {title: '登入'});
+    }
+    else {
+        res.render('account', {title: '我的账户'});
+    }
 });
 
 // 账户页面
 app.get('/account', function(req, res) {
     // 如果没有登入需要redirec到登入页面
-    res.render('account', {title: '我的账户'});
+    if (!req.session.user) {
+        res.redirect('/signin');
+    }
+    else {
+        res.render('account', {title: '我的账户'});
+    }
 });
 
 // 选择进入页面
 app.get('/select', function(req, res) {
-    // 如果没有登入需要redirec到登入页面
-    res.render('select', {title: '选择'});
+    if (!req.session.user) {
+        res.redirect('/signin');
+    }
+    else {
+        res.render('select', {title: '选择'});
+    }
 });
 
 // 选择进入页面
 app.get('/logout', function(req, res) {
     // 如果没有登入需要redirec到登入页面
     res.render('select', {title: '选择'});
+});
+
+// API处理接口
+app.get('/api/*', function(req, res) {
+    // 如果没有登入需要redirec到登入页面
+    res.json({
+        type: 'this is get api'
+    });
+});
+app.post('/api/*', function(req, res) {
+    // 如果没有登入需要redirec到登入页面
+    if (req.path === '/api/signin') {
+        const { login_id, channel, login_info } = req.body;
+        // 简单的验证逻辑
+        if (login_info.email && login_info.password) {
+            // 登录成功，将用户信息保存到session中
+            req.session.user = login_info.email;
+            res.json({errno: 0, message: '登录成功'});
+        } else {
+            // 登录失败
+            res.status(401).json({ message: '用户名或密码错误' });
+        }
+    }
 });
 
 // 启动服务，监听 3000 端口
--- public/js/signin.js
+++ public/js/signin.js
@@ -1,1 +1,36 @@
-console.log('this is login.')
+
+const signinForm = document.querySelector('#sigin-form');
+
+function handleSubmit(event) {
+    event.preventDefault(); // 阻止默认提交事件
+    // 在这里可以执行自定义的提交逻辑
+    const username = signinForm.querySelector('[name="email"]').value;
+    const password = signinForm.querySelector('[name="password"]').value;
+    
+    
+    const requestOptions = {
+        method: 'POST',
+        credentials: 'include',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            login_id: 'login_id_123123123',
+            channel: 'email',
+            login_info: {
+                password: '123456',
+                email: 'test@163.com'
+            }
+        })
+    };
+    fetch('./api/signin', requestOptions)
+    .then(response => response.json())
+    .then(({msg, errno}) => {
+        if (errno === 0) {
+            alert('登录成功');
+            window.location.href = './select';
+        }
+        else {
+            alert('登录失败' + msg);
+        }
+    });
+
+}
